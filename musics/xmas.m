8b5e4263d88ec08463c39d6ae8ac8ca06969a858
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2016-12-22 17:30:35 +0900

    Add X'mas song and replace c8 to cn

---
+++ musics/xmas.m
@@ -0,0 +1,3 @@
+e e e - e e e - e g c > - d < e - - - f f f > - f < f e e e g g f d c - - -
+g > > a - - g < < e cn a g - - > g a g a < g cn b - - -
+> > f - - g - - - f < < d b a g - - > g a g a < g b cn
